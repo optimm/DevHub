8f1e519b6773bafac021db27ca08660fab2c5570
optimm <ayushsaxena823@gmail.com>
2022-12-07 16:23:35 +0530

    search project by owner name email

--- backend/utils/searchProject.js
+++ backend/utils/searchProject.js
@@ -4,21 +4,32 @@ const paginate = require("./paginate");
 const searchProject = async (req, res, searchQuery) => {
   const { q, tags, sortByLikes, sortByComments } = req.query;
   let queryObject = { ...searchQuery };
-  const titleQuery = { title: { $regex: q, $options: "i" } };
   const tagQuery = { tags: { $in: tags } };
 
-  if (q) {
-    queryObject = { ...queryObject, ...titleQuery };
-  }
+  // const titleQuery = { title: { $regex: q, $options: "i" } };
+  // if (q) {
+  //   queryObject = { ...queryObject, ...titleQuery };
+  // }
+
   if (tags && tags.length > 0) {
     queryObject = { ...queryObject, ...tagQuery };
   }
+
   let mongoQuery = Project.find(queryObject).populate(
     "owner",
     "name email avatar"
   );
+
   let data = await paginate(req, res, mongoQuery);
-  const total = await Project.countDocuments(queryObject);
+  if (q) {
+    data = data.filter(
+      (item) =>
+        item.title.includes(q) ||
+        item.owner.name.includes(q) ||
+        item.owner.email.includes(q)
+    );
+  }
+  const total = data.length;
   return { data, total };
 };
 
